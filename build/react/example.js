d859f514be486b89126fd9dfb3c87e2bab3f4734
simison <simison@users.noreply.github.com>
2026-03-06 15:09:52 +0000

    Social logos: fix build by using tsgo instead of tsc (#47482)
    Co-authored-by: tbradsha <32492176+tbradsha@users.noreply.github.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/22768588263
    
    Upstream-Ref: Automattic/jetpack@d1c68285613a48619609c308d519d90c7bfe5eba

--- build/react/example.js
+++ build/react/example.js
@@ -20,7 +20,8 @@ function SocialLogoItemExample({ name, iconSize, showIconNames }) {
         const code = `<SocialLogo icon="${name}" size="${iconSize}" />`;
         window.prompt('Copy component code:', code);
     }, [iconSize, name]);
-    return ((0, jsx_runtime_1.jsxs)("div", { children: [(0, jsx_runtime_1.jsx)(social_logo_1.SocialLogo, { icon: name, size: iconSize, onClick: handleClick }), showIconNames && (0, jsx_runtime_1.jsx)("p", { children: name })] }, name));
+    return (jsx_runtime_1.jsxs("div", { children: [
+            jsx_runtime_1.jsx(social_logo_1.SocialLogo, { icon: name, size: iconSize, onClick: handleClick }), showIconNames && jsx_runtime_1.jsx("p", { children: name })] }, name));
 }
 /**
  * An example React component that displays all the social logos.
@@ -37,7 +38,19 @@ function SocialLogosExample() {
     const handleIconNamesToggle = (0, react_1.useCallback)(e => {
         setShowIconNames(e.target.checked);
     }, [setShowIconNames]);
-    const allSocialLogos = social_logo_data_1.SocialLogoData.map(logo => ((0, jsx_runtime_1.jsx)(SocialLogoItemExample, { name: logo.name, iconSize: iconSize, showIconNames: showIconNames }, logo.name)));
-    return ((0, jsx_runtime_1.jsxs)("div", { className: "social-logos-example", children: [(0, jsx_runtime_1.jsx)("h1", { children: "Social Logos" }), (0, jsx_runtime_1.jsxs)("div", { className: "display-control-group", children: [(0, jsx_runtime_1.jsxs)("div", { className: "display-control", children: [(0, jsx_runtime_1.jsx)("h4", { children: "Small icons" }), (0, jsx_runtime_1.jsxs)("label", { className: "switch", htmlFor: "useSmallIcons", children: [(0, jsx_runtime_1.jsx)("input", { id: "useSmallIcons", type: "checkbox", onChange: handleSmallIconsToggle, checked: useSmallIcons }), (0, jsx_runtime_1.jsx)("span", { className: "handle" })] })] }), (0, jsx_runtime_1.jsxs)("div", { className: "display-control", children: [(0, jsx_runtime_1.jsx)("h4", { children: "Icon names" }), (0, jsx_runtime_1.jsxs)("label", { className: "switch", htmlFor: "showIconNames", children: [(0, jsx_runtime_1.jsx)("input", { id: "showIconNames", type: "checkbox", onChange: handleIconNamesToggle, checked: showIconNames }), (0, jsx_runtime_1.jsx)("span", { className: "handle" }), (0, jsx_runtime_1.jsx)("span", { className: "switch-label", "data-on": "On", "data-off": "Off" })] })] })] }), (0, jsx_runtime_1.jsx)("div", { className: "icons", children: allSocialLogos }), (0, jsx_runtime_1.jsx)("p", { children: (0, jsx_runtime_1.jsx)("a", { href: "https://github.com/Automattic/social-logos", children: "GitHub" }) })] }));
+    const allSocialLogos = social_logo_data_1.SocialLogoData.map(logo => (jsx_runtime_1.jsx(SocialLogoItemExample, { name: logo.name, iconSize: iconSize, showIconNames: showIconNames }, logo.name)));
+    return (jsx_runtime_1.jsxs("div", { className: "social-logos-example", children: [
+            jsx_runtime_1.jsx("h1", { children: "Social Logos" }), jsx_runtime_1.jsxs("div", { className: "display-control-group", children: [
+                    jsx_runtime_1.jsxs("div", { className: "display-control", children: [
+                            jsx_runtime_1.jsx("h4", { children: "Small icons" }), jsx_runtime_1.jsxs("label", { className: "switch", htmlFor: "useSmallIcons", children: [
+                                    jsx_runtime_1.jsx("input", { id: "useSmallIcons", type: "checkbox", onChange: handleSmallIconsToggle, checked: useSmallIcons }), jsx_runtime_1.jsx("span", { className: "handle" })
+                                ] })
+                        ] }), jsx_runtime_1.jsxs("div", { className: "display-control", children: [
+                            jsx_runtime_1.jsx("h4", { children: "Icon names" }), jsx_runtime_1.jsxs("label", { className: "switch", htmlFor: "showIconNames", children: [
+                                    jsx_runtime_1.jsx("input", { id: "showIconNames", type: "checkbox", onChange: handleIconNamesToggle, checked: showIconNames }), jsx_runtime_1.jsx("span", { className: "handle" }), jsx_runtime_1.jsx("span", { className: "switch-label", "data-on": "On", "data-off": "Off" })
+                                ] })
+                        ] })
+                ] }), jsx_runtime_1.jsx("div", { className: "icons", children: allSocialLogos }), jsx_runtime_1.jsx("p", { children: jsx_runtime_1.jsx("a", { href: "https://github.com/Automattic/social-logos", children: "GitHub" }) })
+        ] }));
 }
 exports.default = SocialLogosExample;
